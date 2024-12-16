dafa55d310a070a6d276e1fc343eaca3fcc4386b
wsantos1099 <136863127+wsantos1099@users.noreply.github.com>
2024-12-16 16:14:05 -0300

    Update CheckCompliantSCCM.ps1

--- CheckCompliantSCCM.ps1
+++ CheckCompliantSCCM.ps1
@@ -1,8 +1,15 @@
-﻿Invoke-CimMethod -Namespace 'root\ccm' -ClassName 'SMS_Client' -MethodName 'TriggerSchedule' -Arguments @{sScheduleID='{00000000-0000-0000-0000-000000000113}'}
+#Force Policies
 
+#Force machine Policy
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000021}"
 
-$logFilePath = "C:\Windows\CCM\Logs\UpdatesStore.log"
-$MissingUpdates = Get-Content -Path $logFilePath | Where-Object { $_ -like "*were loaded.*" }
+#Force Software Updates Scan
+Invoke-WmiMethod -Namespace root\ccm -Class sms_client -Name TriggerSchedule "{00000000-0000-0000-0000-000000000113}"
+#Invoke-CimMethod -Namespace 'root\ccm' -ClassName 'SMS_Client' -MethodName 'TriggerSchedule' -Arguments @{sScheduleID='{00000000-0000-0000-0000-000000000113}'}
+
+#Check the Log for missing updates
+$logFilePath = "C:\Windows\CCM\Logs\UpdatesStore-20241216-160824.log"
+$MissingUpdates = Get-Content -Path $logFilePath | Where-Object { $_ -like "*Status=Missing*" }
 
 if ($MissingUpdates) {
     $true
