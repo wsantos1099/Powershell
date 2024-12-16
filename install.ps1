31a1307d11572204fdd90ac020db5a53ea888a4e
wsantos1099 <136863127+wsantos1099@users.noreply.github.com>
2024-12-16 15:19:33 -0300

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,11 @@
+﻿Invoke-CimMethod -Namespace 'root\ccm' -ClassName 'SMS_Client' -MethodName 'TriggerSchedule' -Arguments @{sScheduleID='{00000000-0000-0000-0000-000000000113}'}
+
+
+$logFilePath = "C:\Windows\CCM\Logs\UpdatesStore.log"
+$MissingUpdates = Get-Content -Path $logFilePath | Where-Object { $_ -like "*were loaded.*" }
+
+if ($MissingUpdates) {
+    $true
+} else {
+    $false
+}
